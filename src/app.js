d6e3e459d2fdd4326c422530fd527b30162d26ec
naveenb021 <shoplytic07@gmail.com>
2026-03-08 14:34:09 +0530

    Import React along with hooks in app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const GENRES = ["All", "Action", "Comedy", "Drama", "Horror", "Sci-Fi", "Romance", "Thriller", "Animation", "Documentary"];
 
